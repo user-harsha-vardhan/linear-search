994ce456c12052529c671a3abd432d72546dc6e8
Harshavardhan <harshagowdaout987@gmail.com>
2025-08-19 09:20:17 +0530

    modified println()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val list = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+    val list = intArrayOf(1, 2, 3, 4, 5, 6, 7,8)
     println("enter number to search in the list:")
     val input = readln().toInt()
     val result = linearSearch(list, input)
     if (result != -1)
-        println(result)
+        println("the number exist in the list $result")
     else
         println("not found")
 }
