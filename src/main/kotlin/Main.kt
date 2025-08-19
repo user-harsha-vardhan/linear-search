1cf3f0ab0386dedd49bbd6139a07b90f42d5e7f3
Harshavardhan <harshagowdaout987@gmail.com>
2025-08-19 09:14:57 +0530

    modified funck

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 fun main() {
+    val list = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+    println("enter number to search in the list:")
+    val input = readln().toInt()
+    val result = linearSearch(list, input)
 
 }
-fun linearSearch (initial: IntArray, target: Int): Int {
-    for (i in initial) {
+
+fun linearSearch(initial: IntArray, target: Int): Int {
+    for (i in initial.indices) {
         if (i == target)
             return i
     }
-return -1
+    return -1
 }
